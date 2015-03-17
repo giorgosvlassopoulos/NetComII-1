cefcd1d999d38c4322596526b7a6feca2d465350
ralamarti <ralamarti@gmail.com>
2015-03-17 15:27:26 +0100

    Changed the port at the server

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -32,36 +32,36 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
         try{
- Socket sock = new Socket("localhost", 4444);
+            Socket sock = new Socket("localhost", 4444);
 
- BufferedReader instream = new BufferedReader (new InputStreamReader
-(sock.getInputStream()));
- BufferedWriter outstream = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+            BufferedReader instream = new BufferedReader (new InputStreamReader(sock.getInputStream()));
+            BufferedWriter outstream = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
 
- System.out.println("Sending Messages to the Server...");
-System.out.println("Connecting to "+ sock.getInetAddress()+ " and port "+sock.getPort());
- System.out.println("Local Address :"+sock.getLocalAddress()+" Port"+sock.getLocalPort());
- String strin, strout;
+            System.out.println("Sending Messages to the Server...");
+            System.out.println("Connecting to "+ sock.getInetAddress()+ " and port "+sock.getPort());
+            System.out.println("Local Address :"+sock.getLocalAddress()+" Port"+sock.getLocalPort());
+            String strin, strout;
 
- Scanner in = new Scanner(System.in);
- do{
- System.out.print("Write what the client will send: ");
- strout = in.nextLine();
- outstream.write(strout+"\n");
- outstream.flush();
- strin = instream.readLine();
- System.out.println("The server says: "+strin);
+            Scanner in = new Scanner(System.in);
+            
+            do{
+                System.out.print("Write what the client will send: ");
+                strout = in.nextLine();
+                outstream.write(strout+"\n");
+                outstream.flush();
+ 
+                strin = instream.readLine();
+                System.out.println("The server says: "+strin);
+            }
+ 
+            while (!strin.equals("bye"));
+            instream.close();
+            outstream.close();
+            sock.close();
+            System.out.println("Connection Closing...");
+        
+        } catch (Exception ex){
+            System.out.println("Connection Refused!!!");
+        }
  }
- while (!strin.equals("bye"));
- instream.close();
- outstream.close();
- sock.close();
- System.out.println("Connection Closing...");
- }
- catch (Exception ex){
- System.out.println("Connection Refused!!!");
-
- }
- }
-
 }
