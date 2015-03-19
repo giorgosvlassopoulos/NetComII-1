e70134827f6ddba571dfde3fd94ba541c6108c61
davix16 <davix1603@gmail.com>
2015-03-19 10:40:54 +0100

    Update with changes sent my Giorgos to emails.

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,67 +1,85 @@
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Client {
 
     private static Socket sock;
-    private static String fileName;
-    private static BufferedReader stdin;
-    private static PrintStream os;
-    public static String sentence;
-    public static String modifiedSentence;
-    public static BufferedReader inFromUser = null;
-    public static DataOutputStream outToServer = null;
-    public static BufferedReader inFromServer = null;
 
     public static void main(String[] args) throws IOException {
-        try{
-            Socket sock = new Socket("localhost", 25);
+        try {
+            sock = new Socket("localhost", 25);
 
-            BufferedReader instream = new BufferedReader (new InputStreamReader(sock.getInputStream()));
+            BufferedReader instream = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             BufferedWriter outstream = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+            //initialize the BufferedReader and Writer
 
-            System.out.println("Sending Messages to the Server...");
-            System.out.println("Connecting to "+ sock.getInetAddress()+ " and port "+sock.getPort());
-            System.out.println("Local Address :"+sock.getLocalAddress()+" Port"+sock.getLocalPort());
             String strin, strout;
 
-            Scanner in = new Scanner(System.in);
-            
-            do{
-                System.out.print("Write what the client will send: ");
-                strout = in.nextLine();
-                outstream.write(strout+"\n");
-                outstream.flush();
- 
-                strin = instream.readLine();
-                System.out.println("The server says: "+strin);
-            }
- 
-            while (!strin.equals("bye"));
+            //Scanner in = new Scanner(System.in);
+            strin = instream.readLine();
+            System.out.println(strin);
+            //1st received message of the client
+            strout = "HELO" + " " + sock.getInetAddress().getHostName() + "\r\n";
+            outstream.write(strout);
+            outstream.flush();
+            //1st sent message of the client
+            strin = instream.readLine();
+            System.out.println(strin);
+            //2d received message of the client
+            //Starts the protocol for email
+            String username = "sender";
+            String username2 = "receiver";
+            String subject = "test";
+            String message = "message";
+            //   do{
+            strout = "MAIL" + " " + "FROM:" + " " + username + "@" + sock.getInetAddress() + "\r\n";
+            outstream.write(strout);
+            outstream.flush();
+            //2d sent message of the client
+            strin = instream.readLine();
+            System.out.println(strin);
+            //3d received message of the client
+            strout = "RCPT" + " " + "TO:" + " " + username2 + "@" + sock.getInetAddress() + "\r\n";
+            outstream.write(strout);
+            outstream.flush();
+            //3d sent message of the client
+            strin = instream.readLine();
+            System.out.println(strin);
+            //4th received message of the client
+            strout = "DATA \r\n";
+            outstream.write(strout);
+            outstream.flush();
+            //4th sent message of the client
+            strin = instream.readLine();
+            System.out.println(strin);
+                    //5th received message of the client
+
+            /* strout="Subject:"+subject+"\r\n"+"From:"+username+"\r\n"+"To:"+username2+"\r\n";
+             outstream.write(strout);
+             outstream.flush();
+             strout="\n";
+             outstream.write(strout);
+             outstream.flush();
+             strout=message+"\r\n"+"."+"\r\n";
+             outstream.write(strout);
+             outstream.flush();
+             strin = instream.readLine();
+             System.out.println(strin);
+             */
+            //} while (!strin.equals("QUIT \r\n"));
             instream.close();
             outstream.close();
             sock.close();
             System.out.println("Connection Closing...");
-        
-        } catch (Exception ex){
+
+        } catch (Exception ex) {
             System.out.println("Connection Refused!!!");
         }
- }
+    }
 }
